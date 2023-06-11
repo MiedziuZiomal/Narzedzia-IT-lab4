3ea30e4317806329a65a522c7048e70081563998
Miedziu <krzysztofmiedziak2000@gmail.com>
2023-06-11 14:51:32 +0200

    advanced functions with flags

--- index.sh
+++ index.sh
@@ -1,12 +1,10 @@
-# mdevelopmentBranch
-
 #!/bin/bash
 
 function print_date {
     echo $(date)
 }
 
-function create_files {
+function create_logs {
     local number=$1
     if [[ -z "$number" ]]; then
         number=100
@@ -17,16 +15,59 @@ function create_files {
     done
 }
 
-function print_help {
-    echo "Opcje skryptu:"
-    echo "--date: wyświetla aktualną datę"
-    echo "--logs [number]: tworzy pliki logx.txt, gdzie x to numer pliku od 1 do 100. Jeśli podano liczbę, tworzy tyle plików."
-    echo "--help: wyświetla pomoc"
+function create_errors {
+    local number=$1
+    if [[ -z "$number" ]]; then
+        number=100
+    fi
+
+    mkdir -p errorx
+
+    for ((i=1;i<=number;i++)); do
+        echo -e "Nazwa pliku: errorx/error$i.txt\nNazwa skryptu: $0\nData: $(date)" > errorx/error$i.txt
+    done
 }
 
-case $1 in
-    --date) print_date ;;
-    --logs) create_files $2 ;;
-    --help) print_help ;;
-    *) echo "Nieznana opcja: $1. Użyj --help, aby uzyskać pomoc." ;;
-esac
+function clone_repo {
+    local dir=$(pwd)
+    git clone <url-do-repozytorium> $dir
+    export PATH=$dir:$PATH
+}
+
+function print_help {
+    echo "Opcje skryptu:"
+    echo "-d, --date: wyświetla aktualną datę"
+    echo "-l, --logs [number]: tworzy pliki logx.txt, gdzie x to numer pliku od 1 do 100. Jeśli podano liczbę, tworzy tyle plików."
+    echo "-e, --error [number]: tworzy pliki errorx/errorx.txt, gdzie x to numer pliku od 1 do 100. Jeśli podano liczbę, tworzy tyle plików."
+    echo "--init: klonuje repozytorium do katalogu, w którym uruchomiony jest skrypt, i dodaje ten katalog do zmiennej środowiskowej PATH."
+    echo "-h, --help: wyświetla pomoc"
+}
+
+while (( "$#" )); do
+  case "$1" in
+    -d|--date)
+      print_date
+      shift
+      ;;
+    -l|--logs)
+      create_logs $2
+      shift 2
+      ;;
+    -e|--error)
+      create_errors $2
+      shift 2
+      ;;
+    --init)
+      clone_repo
+      shift
+      ;;
+    -h|--help)
+      print_help
+      shift
+      ;;
+    *)
+      echo "Nieznana opcja: $1. Użyj -h lub --help, aby uzyskać pomoc."
+      shift
+      ;;
+  esac
+done
